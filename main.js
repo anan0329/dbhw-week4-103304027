93eaca1076eb92f3c97f68dd62cb6931ec1c2a62
anan0329 <anan0329@yahoo.com.tw>
2016-04-06 18:02:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=bf073841-c734-49bf-a97f-3757a6013812";
+var API_PATH = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=38f2d8e0-07ce-4201-87cb-3462be39d8aa";
 function getData(cb) {
   $.ajax({
     url : API_PATH,
